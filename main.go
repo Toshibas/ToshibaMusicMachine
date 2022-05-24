064d16ed12a7ddf75865284889a8e754927c0894
Bohdan Mandryk <mehanika@mehanika.intrak.tuke.sk>
2022-05-24 18:53:01 +0200

    bot init

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 
 func main() {
 
-	fmt.Println("This is a best music bot!")
+	bot, err := discordgo.New("Bot " + "OTc4NjkxNDk2MTkyNjU5NDU2.GwS474.IVRh60zMfVZqWSeL87ziEg_kxRhxm2tTYkGg1I")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = bot.Open()
+
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	fmt.Println("ToshibaMusicMachine.  Press CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	bot.Close()
 
 }
