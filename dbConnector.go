0cd5f1ff9a60471b1ecc6be2bd574a1349f011ef
Anton Khimiak <ximyko@gmail.com>
2022-06-03 00:31:28 +0300

    db connector functionality

--- dbConnector.go
+++ dbConnector.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+
+	"log"
+	"fmt"
+
+	"database/sql"
+    _ "github.com/go-sql-driver/mysql"
+
+)
+
+type DBConnector struct {
+	address string
+	port string
+	user string
+	password string
+	database string
+	db *sql.DB 
+}
+
+func createDBConnector(address, port, user, password, database string) (DBConnector, error) {
+
+	connectionRow := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, address, port, database)
+
+	db, err := sql.Open("mysql", connectionRow)
+
+	if err != nil {
+		return DBConnector{}, err
+	}
+
+	connector := DBConnector{
+		address: address,
+		port: port,
+		user: user,
+		password: password,
+		database: database,
+		db: db,
+	}
+	
+	return connector, nil
+}
+
+func (connector DBConnector) closeConnection() {
+
+	connector.db.Close()
+
+}
+
+func (connector DBConnector) guildExists(guildID string) bool {
+
+	rows, err := connector.db.Query("SELECT * FROM guilds WHERE id = ?", guildID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	return rows.Next() 
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	
+	"fmt"
+	"log"
+
+)
+
+ 
+func main() {
+
+	dbConnector, err := createDBConnector("54.226.233.122", "3306", "bodko", "1234", "toshibaMusicMachine")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(dbConnector.guildExists("qwertyuiop"))
+	fmt.Println(dbConnector.guildExists("qwertyuioq"))
+
+	dbConnector.closeConnection()
+
+}
