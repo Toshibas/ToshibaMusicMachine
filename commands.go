6e025254e019d64cb5ec8dec3115e16386f0068d
Bohdan Mandryk <bohdan.mandryk.2001@gmail.com>
2022-06-17 14:11:08 +0200

    commands.go

---
+++ commands.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/bwmarrin/discordgo"
+
+func Play(s *discordgo.Session) {
+
+}
+
+func Pause(s *discordgo.Session) {
+
+}
+
+func Stop(s *discordgo.Session) {
+
+}
+
+func Add(s *discordgo.Session) {
+
+}
+
+func Prev(s *discordgo.Session) {
+
+}
+
+func Next(s *discordgo.Session) {
+
+}
